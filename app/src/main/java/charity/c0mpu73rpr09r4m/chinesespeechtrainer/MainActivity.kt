e0a4b2cebfa95b16c7a644b0de2c7e26932121d0
Daniel Ohm <daniel.edward.ohm.jr@gmail.com>
2025-11-20 08:01:16 -0800

    added spinner

--- app/src/main/java/charity/c0mpu73rpr09r4m/chinesespeechtrainer/MainActivity.kt
+++ app/src/main/java/charity/c0mpu73rpr09r4m/chinesespeechtrainer/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import charity.c0mpu73rpr09r4m.chinesespeechtrainer.ui.theme.ChineseSpeechTrainerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ChineseSpeechTrainerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "World",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    InitializeMain( modifier = Modifier.padding(innerPadding) )
                 }
             }
         }
@@ -31,17 +31,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun InitializeMain(modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChineseSpeechTrainerTheme {
-        Greeting("World")
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Main",
+            modifier = Modifier.align(Alignment.Center),
+            fontSize = 32.sp
+        )
     }
 }
---
+++ app/src/main/java/charity/c0mpu73rpr09r4m/chinesespeechtrainer/StartActivity.kt
@@ -0,0 +1,90 @@
+package charity.c0mpu73rpr09r4m.chinesespeechtrainer
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import charity.c0mpu73rpr09r4m.chinesespeechtrainer.ui.theme.ChineseSpeechTrainerTheme
+
+class StartActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChineseSpeechTrainerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    InitializeStart( modifier = Modifier.padding(innerPadding) )
+                }
+            }
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 3000)
+    }
+}
+
+@Composable
+fun InitializeStart(modifier: Modifier = Modifier) {
+    val fontFamily = FontFamily(Font(R.font.go3v2))
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Text(
+                text = "Chinese Speech Trainer",
+                fontSize = 32.sp,
+                style = TextStyle(
+                    fontFamily = fontFamily,
+                    color = Color.Green
+                )
+            )
+            Spacer(modifier = Modifier.height(25.dp))
+            LinearProgressIndicator(
+                color = Color.Green,
+                trackColor = Color.Green,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp)
+            )
+        }
+        Text(
+            text = "© C0MPU73R PR09R4M CHARITY",
+            modifier = Modifier.align(Alignment.BottomCenter),
+            style = TextStyle(
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
